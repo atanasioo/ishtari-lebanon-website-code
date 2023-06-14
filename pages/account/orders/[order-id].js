46ea49f5c9f91c21a1b96c047ef6aa44913bd7ab
FatmeHassan <fatimahasan.ishtari@gmail.com>
2023-06-14 13:57:51 +0300

    order detail page

---
+++ pages/account/orders/[order-id].js
@@ -0,0 +1,307 @@
+import { axiosServer } from "@/axiosServer";
+import useDeviceSize from "@/components/useDeviceSize";
+import { AccountContext } from "@/contexts/AccountContext";
+import buildLink from "@/urls";
+import dynamic from "next/dynamic";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useContext, useEffect, useState } from "react";
+import { AiOutlineCar } from "react-icons/ai";
+import { BiBox } from "react-icons/bi";
+import { BsCart2 } from "react-icons/bs";
+import { HiOutlineHome } from "react-icons/hi";
+
+function OrderDetails() {
+    const router = useRouter();
+  const [data, setData] = useState();
+  const [email, setEmail] = useState("");
+  let id  = router.query["order-id"];
+  const [width, height] = useDeviceSize();
+  const [loading, setLoading] = useState(true);
+  const [state, dispatch] = useContext(AccountContext);
+  const [success, setSuccess] = useState(false);
+
+  const PointsLoader = dynamic(() => import("@/components/PointsLoader"), {
+    ssr: false, // Disable server-side rendering
+  });
+
+//   useEffect(() => {
+//     if (!state.loged) {
+//       dispatch({ type: "setShowOver", payload: true });
+//       dispatch({ type: "setShowLogin", payload: true });
+//     } else {
+//       dispatch({ type: "setShowOver", payload: false });
+//       dispatch({ type: "setShowLogin", payload: false });
+//     }
+//   }, [state.loged, loading, success]);
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+
+    axiosServer
+      .get(buildLink("get_account", undefined, window.innerWidth))
+      .then((response) => {
+        setEmail(response?.data?.data?.email);
+        setLoading(false);
+        if (response.data.success) {
+          setSuccess(true);
+        }
+      });
+    axiosServer
+      .get(buildLink("order_details", undefined, window.innerWidth) + id)
+      .then((response) => {
+        if (response.data.success) {
+          setData(response?.data.data);
+          setLoading(false);
+          setSuccess(true);
+        }
+      });
+  }, [id]);
+
+  return (
+    <div>
+      {loading ? (
+        <PointsLoader />
+      ) : (
+        state.loged && (
+          <div className="px-2 pb-5 md:px-0 md:mx-24 md:pb-14">
+            <div className="flex justify-between pt-5 text-sm font-bold">
+              <p>#{data?.order_id}</p>
+              <p>{data?.date_added}</p>
+            </div>
+            <div className="main_container">
+              <div className="container padding-bottom-3x mb-1">
+                <div className="card mb-3">
+                  <div className="p-4 text-center text-white text-lg bg-dark rounded-top">
+                    {/* <span className="text-uppercase">Tracking Order No - </span>
+                    <span className="text-medium">001698653lp</span> */}
+                  </div>
+
+                  <div className="card-body">
+                    <div className="flex steps d-flex flex-wrap flex-sm-nowrap justify-content-between padding-top-2x padding-bottom-1x">
+                      <div
+                        className={`step  w-1/5 text-center ${
+                          data &&
+                          data?.order_status[0]?.status?.indexOf(
+                            "Order Placed"
+                          ) > -1 &&
+                          "completed"
+                        }`}
+                      >
+                        <div className="step-icon-wrap ">
+                          <div className="step-icon">
+                            <BsCart2 className="text-d18 m-2.5" />
+                          </div>
+                        </div>
+                        <h4 className="step-title">Order Placed</h4>
+                      </div>
+                      <div
+                        className={`step  w-1/5 text-center ${
+                          data?.order_status[1]?.status?.indexOf("Ready") >
+                            -1 && "completed"
+                        }`}
+                      >
+                        <div className="step-icon-wrap">
+                          <div className="step-icon">
+                            <BiBox className="text-d18 m-2.5" />
+                          </div>
+                        </div>
+                        <h4 className="step-title">Ready</h4>
+                      </div>
+                      <div
+                        className={`step  w-1/12 text-center ${
+                          data?.order_status[1]?.status?.indexOf("Ready") >
+                            -1 && "completed"
+                        }`}
+                      >
+                        <div className="step-icon-wrap"></div>
+                      </div>
+                      <div
+                        className={`step  w-1/12 text-center ${
+                          data?.order_status[2]?.status?.indexOf("Shipped") >
+                            -1 && "completed"
+                        }`}
+                      >
+                        <div className="step-icon-wrap"></div>
+                      </div>
+
+                      <div
+                        className={`step  w-1/5 text-center ${
+                          data?.order_status[2]?.status?.indexOf("Shipped") >
+                            -1 && "completed"
+                        }`}
+                      >
+                        <div className="step-icon-wrap">
+                          <div className="step-icon">
+                            <AiOutlineCar className="text-d18 m-2.5" />
+                          </div>
+                        </div>
+                        <h4 className="step-title">Pickup</h4>
+                      </div>
+                      <div
+                        className={`step  w-1/5 text-center ${
+                          data?.order_status[3]?.status?.indexOf("Delivered") >
+                            -1 && "completed"
+                        }`}
+                      >
+                        <div className="step-icon-wrap">
+                          <div className="step-icon ">
+                            <HiOutlineHome className="text-d18 m-2.5" />
+                          </div>
+                        </div>
+                        <h4 className="step-title">Delivered</h4>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-5">
+              <p className="text-xs font-semibold">
+                {data?.firstname + " "}
+                {data?.lastname}
+              </p>
+              <p className="text-xs">{data?.telephone}</p>
+              <p className="text-xxs text-gray-600">{email}</p>
+            </div>
+
+            <div className="flex-row md:flex mt-5 pb-7  border-b  border-dgrey1 justify-between">
+              <div className="w-full md:w-1/2 border  border-dgrey shadow-lg mr-5 rounded-md">
+                {/* right */}
+                <p className="cart-header">Shipping Address</p>
+                <div className="cart-body">
+                  <p className="text-sm py-5 mx-3 rounded-t-md border-b text-dblack border-dgrey1">
+                    {data?.firstname + " "}
+                    {data?.lastname}
+                  </p>
+                  <p
+                    className="mt-5 text-sm mx-3 pb-2"
+                    dangerouslySetInnerHTML={{ __html: data?.shipping_address }}
+                  ></p>
+                </div>
+              </div>
+              <div className="w-full md:w-1/2 border mt-3 md:mt-0 border-dgrey shadow-lg rounded-md">
+                {/* right */}
+                <p className="cart-header">Payment Address</p>
+                <div className="cart-body">
+                  <p className="text-sm py-5 mx-3 rounded-t-md border-b text-dblack border-dgrey1">
+                    {data?.firstname + " "}
+                    {data?.lastname}
+                  </p>
+                  <p
+                    className="mt-5 text-sm mx-3 pb-2 "
+                    dangerouslySetInnerHTML={{ __html: data?.payment_address }}
+                  ></p>
+                </div>
+              </div>
+            </div>
+
+            {/* TABLE */}
+            <div className="mt-7">
+              {width > 650 ? (
+                <table className="w-full  text-left">
+                  <thead className="border">
+                    <th className=" border-l  px-4  py-1 text-sm">#</th>
+                    <th className="border-l px-2 md:px-4 py-1  text-sm">
+                      Product
+                    </th>
+
+                    <th className="border-l px-4 md:px-4 py-1  text-sm">SKU</th>
+                    <th className="border-l px-2 md:px-4 py-1  text-sm">
+                      Quantity
+                    </th>
+                    <th className="border-l px-2 md:px-4 py-1  text-sm">
+                      Price
+                    </th>
+                    <th className="px-2 border-l md:px-4  py-1 text-sm">
+                      Total
+                    </th>
+                  </thead>
+                  {data?.products?.map((data) => (
+                    <tr className="border">
+                      <td className="border  px-4">
+                        <Link href={`/product/${data.product_id}`}>
+                            <img className="w-12" src={data.image} alt={data.name} />
+                        </Link>
+                      </td>
+                      <td className="px-4 text-sm">
+                        <span dangerouslySetInnerHTML={{__html: data.name}}></span>
+                        
+                        <span className="font-semibold">
+                          {" "}
+                          {data?.option[0]?.name &&
+                            "( " + data?.option[0]?.value + " )"}{" "}
+                        </span>{" "}
+                      </td>
+
+                      <td className="border   px-4 text-sm">{data.model}</td>
+                      <td className="border  px-4 text-sm">{data.quantity}</td>
+                      <td className="border  px-4 text-sm">{data.price}</td>
+                      <td className="border  px-4 text-sm"> {data.total}</td>
+                    </tr>
+                  ))}
+                </table>
+              ) : (
+                <table className="w-full  text-left">
+                  <thead className="border">
+                    <div className="hidden md:block">
+                      {" "}
+                      <th className=" border-l  px-4  py-1 text-sm">#</th>
+                    </div>
+                    <th className="border-l px-2  py-1  text-sm">Product</th>
+                    <th className="border-l px-4  py-1  text-sm">SKU</th>
+                    <th className="border-l px-2  py-1  text-sm">Quantity</th>
+                    <th className="border-l px-2  py-1  text-sm">Price</th>
+                    <th className="border-l px-2  py-1 text-sm">Total</th>
+                  </thead>
+                  {data?.products.map((data) => (
+                    <tr className="border">
+                      <div className="hidden md:flex-wrap">
+                        {" "}
+                        <td className="border  px-4">
+                          <img className="w-12" src={data.image} alt="" />
+                        </td>
+                      </div>
+                      <td className=" px-2 md:px-4 text-sm line-clamp-3 md:line-clamp-none">
+                        {data.name}
+                      </td>
+                      <td className="border  px-2 md:px-4 text-sm">
+                        {data.model}
+                      </td>
+                      <td className="border px-2 md:px-4 text-sm">
+                        {data.quantity}
+                      </td>
+                      <td className="border px-2 md:px-4 text-sm">
+                        {data.price}
+                      </td>
+                      <td className="border px-2 md:px-4 text-sm">
+                        {data.total}
+                      </td>
+                    </tr>
+                  ))}
+                </table>
+              )}
+            </div>
+            <div className="flex justify-between mt-5 md:mt-10 border-b border-dgrey1 pb-2 items-centre">
+              {data?.totals?.map((data) => (
+                <div>
+                  <div className="flex-row  ">
+                    <p className="border-b border-dgrey1"> {data.title} </p>
+                  </div>
+                  <div>
+                    <p className="mt-4"> {data.text} </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        )
+      )}
+    </div>
+  );
+}
+
+export default OrderDetails;
--- pages/account/orders/index.js
+++ pages/account/orders/index.js
@@ -10,31 +10,30 @@ import { useRouter } from "next/router";
 import React, { useContext, useEffect, useState } from "react";
 
 function Orders() {
-    const [width, height] = useDeviceSize();
-    const [data, setData] = useState("");
-    const [loading, setLoading] = useState("");
-    const [state, dispatch] = useContext(AccountContext);
-    const router = useRouter();
-    const path ="";
+  const [width, height] = useDeviceSize();
+  const [data, setData] = useState("");
+  const [loading, setLoading] = useState("");
+  const [state, dispatch] = useContext(AccountContext);
+  const router = useRouter();
+  const path = "";
 
-    useEffect(() => {
-        setLoading(true);
-        axiosServer
-          .get(buildLink("orders", undefined, window.innerWidth))
-          .then((response) => {
-            if (response.data.success) {
-              setData(response.data.data);
-              setLoading(false);
-            } else {
-              setLoading(false);
-              dispatch({ type: "setLoading", payload: false });
-              if (!state.loading && !state.loged) {
-                router.push("/");
-              }
-            }
-          });
-      }, []);
-
+  useEffect(() => {
+    setLoading(true);
+    axiosServer
+      .get(buildLink("orders", undefined, window.innerWidth))
+      .then((response) => {
+        if (response.data.success) {
+          setData(response.data.data);
+          setLoading(false);
+        } else {
+          setLoading(false);
+          dispatch({ type: "setLoading", payload: false });
+          if (!state.loading && !state.loged) {
+            router.push("/");
+          }
+        }
+      });
+  }, []);
 
   return (
     <div className="container text-dblack">
@@ -48,95 +47,138 @@ function Orders() {
             )}
           </div>
           <div className="w-full md:w-4/5 px-2 md:px-0 md:pl-8 mb-5">
-              {/* Header */}
-              <div className="flex items-center mb-8 mt-4">
-                <div>
-                  <p className="text-lg font-semibold">Orders</p>
-                  <p className=" font-light">Manage your Order details</p>
-                </div>
+            {/* Header */}
+            <div className="flex items-center mb-8 mt-4">
+              <div>
+                <p className="text-lg font-semibold">Orders</p>
+                <p className=" font-light">Manage your Order details</p>
               </div>
-              {data?.orders?.length !== 0 &&
-                data?.orders?.map((data) => (
-                  <div className="bg-white rounded-md shadow-lg  pb-5">
-                    <div className="md:mt-5  md:pb-5">
-                      <div className="flex flex-col justify-start items-center text-dblue sm:flex-row cart-header text-center sm:text-dblack">
-                        <div className={` w-8/12 ${width > 650 ? 'text-left' : 'text-center'}`}> #{data.order_id}   - {data?.status} </div>
+            </div>
+            {data?.orders?.length !== 0 &&
+              data?.orders?.map((data) => (
+                <div className="bg-white rounded-md shadow-lg  pb-5">
+                  <div className="md:mt-5  md:pb-5">
+                    <div className="flex flex-col justify-start items-center text-dblue sm:flex-row cart-header text-center sm:text-dblack">
+                      <div
+                        className={` w-8/12 ${
+                          width > 650 ? "text-left" : "text-center"
+                        }`}
+                      >
+                        {" "}
+                        #{data.order_id} - {data?.status}{" "}
+                      </div>
 
-                        {(Cookies.get("site-local-name") === "ishtari" ||
-                           window.location.host === "www.ishtari.com")  && data?.status === "Complete 2021"? (
-                            <div className="my-2 text-right justify-end">                    
-                            <Link
-                              className="flex"
-                              href={{
-                                pathname: `${path}/account/order-reviews`,
-                                state: {
-                                  id: data.order_id
-                                }
-                              }}
-                              key={data.order_id}
-                            >
-                            <svg  width="15" height="15" fill="currentColor" className="mt-1" viewBox="0 0 16 16"> <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/> </svg>
-                              <span className=" mx-2 whitespace-nowrap"> Write a review</span>
-                            </Link>             
-                          </div>
-                           ) : (
-                            <div>
-
-                            </div>
-                           )}
-
-                        <div className="mx-4 text-right justify-end ">
+                      {(Cookies.get("site-local-name") === "ishtari" ||
+                        window.location.host === "www.ishtari.com") &&
+                      data?.status === "Complete 2021" ? (
+                        <div className="my-2 text-right justify-end">
                           <Link
-                            className="flex mx-4"
+                            className="flex"
                             href={{
-                              pathname: `${path}/account/order-details`,
+                              pathname: `${path}/account/order-reviews`,
                               state: {
-                                id: data.order_id
-                              }
+                                id: data.order_id,
+                              },
                             }}
                             key={data.order_id}
                           >
-                            {/* <span className="text-dblue">&#x1f441;</span> */}
-                            <span><svg width="19" height="18" viewBox="0 0 24 24" className="mt-1" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M21.2572 10.9622C21.7314 11.5813 21.7314 12.4187 21.2572 13.0378C19.764 14.9868 16.1818 19 12 19C7.81823 19 4.23598 14.9868 2.74284 13.0378C2.26857 12.4187 2.26856 11.5813 2.74283 10.9622C4.23598 9.01321 7.81823 5 12 5C16.1818 5 19.764 9.01321 21.2572 10.9622Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <circle cx="12" cy="12" r="3" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></circle> </svg></span>
-
-                            <span className="mx-1 whitespace-nowrap"> Order details</span>
+                            <svg
+                              width="15"
+                              height="15"
+                              fill="currentColor"
+                              className="mt-1"
+                              viewBox="0 0 16 16"
+                            >
+                              {" "}
+                              <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />{" "}
+                            </svg>
+                            <span className=" mx-2 whitespace-nowrap">
+                              {" "}
+                              Write a review
+                            </span>
                           </Link>
                         </div>
-                      </div>
+                      ) : (
+                        <div></div>
+                      )}
 
-                      <div className="cart-body">
-                        <div className="flex md:px-2 py-1 justify-between items-center mt-4 md:ml-5 bg-white">
-                          <div className="focus:text-dblue flex-row  items-center space-y-4  border-dblue">
-                            <p className="flex space-x-1 text-sm">
-                              <span>Name:</span> <span>{data.name}</span>
-                            </p>
-                            <p className="flex space-x-1 text-sm">
-                              <span className=" text-d12 md:text-base ">
-                                Number of products:
-                              </span>{" "}
-                              <span> {" " + data.products}</span>
-                            </p>
-                          </div>
-                          <div className="flex-row items-center space-y-4 md:mr-52 ">
-                            <p className="text-sm flex space-x-1">
-                              <span>Date Added:</span>{" "}
-                              <span className="font-semibold">
-                                {data.date_added}
-                              </span>
-                            </p>
-                            <p className="text-sm space-x-1">
-                              <span>Total:</span>
-                              <span className="font-semibold">
-                                {data.total}
-                              </span>
-                            </p>
-                          </div>
+                      <div className="mx-4 text-right justify-end ">
+                        <Link
+                          className="flex mx-4"
+                          href={`${path}/account/orders/${data.order_id}`}
+                          key={data.order_id}
+                        >
+                          {/* <span className="text-dblue">&#x1f441;</span> */}
+                          <span>
+                            <svg
+                              width="19"
+                              height="18"
+                              viewBox="0 0 24 24"
+                              className="mt-1"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              {" "}
+                              <path
+                                d="M21.2572 10.9622C21.7314 11.5813 21.7314 12.4187 21.2572 13.0378C19.764 14.9868 16.1818 19 12 19C7.81823 19 4.23598 14.9868 2.74284 13.0378C2.26857 12.4187 2.26856 11.5813 2.74283 10.9622C4.23598 9.01321 7.81823 5 12 5C16.1818 5 19.764 9.01321 21.2572 10.9622Z"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                              ></path>{" "}
+                              <circle
+                                cx="12"
+                                cy="12"
+                                r="3"
+                                stroke="black"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                              ></circle>{" "}
+                            </svg>
+                          </span>
+
+                          <span className="mx-1 whitespace-nowrap">
+                            {" "}
+                            Order details
+                          </span>
+                        </Link>
+                      </div>
+                    </div>
+
+                    <div className="cart-body">
+                      <div className="flex md:px-2 py-1 justify-between items-center mt-4 md:ml-5 bg-white">
+                        <div className="focus:text-dblue flex-row  items-center space-y-4  border-dblue">
+                          <p className="flex space-x-1 text-sm">
+                            <span>Name:</span> <span>{data.name}</span>
+                          </p>
+                          <p className="flex space-x-1 text-sm">
+                            <span className=" text-d12 md:text-base ">
+                              Number of products:
+                            </span>{" "}
+                            <span> {" " + data.products}</span>
+                          </p>
+                        </div>
+                        <div className="flex-row items-center space-y-4 md:mr-52 ">
+                          <p className="text-sm flex space-x-1">
+                            <span className="hidden  md:block">
+                              Date Added:
+                            </span>{" "}
+                            <span className="font-semibold text-dgrey1 md:text-dblack">
+                              {data.date_added}
+                            </span>
+                          </p>
+                          <p className="text-sm space-x-1">
+                            <span>Total:</span>
+                            <span className="font-semibold">{data.total}</span>
+                          </p>
                         </div>
                       </div>
                     </div>
                   </div>
-                ))}
-            </div>
+                </div>
+              ))}
+          </div>
         </div>
       </div>
     </div>
