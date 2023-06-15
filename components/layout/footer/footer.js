6b33363fd357a9df68e0298417f686ef4bbddcdd
omar zrayka <nickobabychoppa@gmail.com>
2023-06-15 14:09:56 +0300

    ss

--- components/layout/footer/footer.js
+++ components/layout/footer/footer.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import dynamic from "next/dynamic";
-
+import {Cookies} from "js-cookie";
+ import buildLink from "@/urls";
+ import { axiosServer } from "@/axiosServer";
 export default function Footer(props) {
-  const data = props.footer_categories;
+  const [result, setResult] = useState();
   const info = props?.information_data?.informations;
+
+  
+  useEffect(()=>{
+    console.log("omar")
+    const  resp =  axiosServer.get(
+      buildLink("footerv2", undefined, undefined),
+      {
+        headers: {
+          Authorization: "Bearer " + Cookies.get("api-token")
+        }
+      }
+    );
+    console.log("resp")
+    console.log(resp)
+    setData(resp.data)
+
+  },)
+
+
+
+  
 
   const FooterPart1 = dynamic(() => import("./footerPart1"), {
     ssr: false // Disable server-side rendering
@@ -17,13 +40,13 @@ export default function Footer(props) {
   });
 
   return (
-    <div >
+    <div>
       <FooterPart1 />
       <div className="border-t-8 mb-4 border-dprimarybg"></div>
 
-      <FooterPartCenter data={data}  />
+      <FooterPartCenter data={result} />
 
-      <FooterPart2 info={info}/>
+      <FooterPart2 info={result} />
     </div>
   );
 }
