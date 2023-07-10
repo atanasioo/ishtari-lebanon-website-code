c6b1615f7a211eaffb1ccdeaa08fc453340a7c27
omar zrayka <nickobabychoppa@gmail.com>
2023-07-10 12:05:21 +0300

    ..

--- components/catalog/CatalogPages.js
+++ components/catalog/CatalogPages.js
@@ -896,7 +896,7 @@ function CatalogPage(props) {
               <div className="p-1">
                 <SingleProduct
                   item={item}
-                  isSlider={false}
+                  isSlider={true}
                   isList={productDisplay === "grid" ? false : true}
                 ></SingleProduct>
               </div>
