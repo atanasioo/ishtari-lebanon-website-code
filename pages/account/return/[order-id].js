ff1a61a856ba1e1f6785a90758e0139dd9296401
FatmeHassan <fatimahasan.ishtari@gmail.com>
2023-09-16 22:07:50 +0300

    /

--- pages/account/return/[order-id].js
+++ pages/account/return/[order-id].js
@@ -1,9 +1,36 @@
+import { axiosServer } from '@/axiosServer';
+import buildLink from '@/urls';
 import { useRouter } from 'next/router';
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function ReturnProducts() {
     const router = useRouter();
     let id = router.query["order-id"];
+
+    useEffect(() => {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+  
+      axiosServer
+        .get(buildLink("get_account", undefined, window.innerWidth))
+        .then((response) => {
+          setLoading(false);
+  
+          if (!response.data.success) {
+            router.push("/");
+          }
+        });
+      axiosServer
+        .get(buildLink("order_details", undefined, window.innerWidth) + id)
+        .then((response) => {
+          if (response.data.success) {
+            setData(response?.data.data);
+            setLoading(false);
+          }
+        });
+    }, [id]);
 
   return (
     <div>ReturnProducts</div>
