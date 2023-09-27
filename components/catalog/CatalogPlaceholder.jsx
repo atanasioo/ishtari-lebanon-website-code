c54c4afa67b9d599f4f64679184293ecd67dbfc5
omar zrayka <nickobabychoppa@gmail.com>
2023-09-27 09:53:39 +0300

    sss

--- components/catalog/CatalogPlaceholder.jsx
+++ components/catalog/CatalogPlaceholder.jsx
@@ -1,6 +1,12 @@
+
+
 function CatalogPlaceholder({style}) {
+
+
     return (
-        <div className={`${style} container pt-2 mb-12`} style={{ backgroundColor: "#f7f7fa" }}>
+
+       <>
+        <div className={`${style} container pt-2 mb-12 hidden mobile:block`} style={{ backgroundColor: "#f7f7fa" }}>
             <div className="placeholder_animation h-8 w-2/12 bg-white mt-4 mb-6"></div>
             <div className="flex">
                 {/* Left */}
@@ -93,6 +99,56 @@ function CatalogPlaceholder({style}) {
                 </div>
             </div>
         </div>
+        
+        <div className="animate-pulse container mb-12 mobile:hidden" style={{ backgroundColor: "#f7f7fa" }}>
+            <div className="placeholder_animation h-8 w-1/2 bg-white mt-4 mb-6"></div>
+            <div className="placeholder_animation h-8 w-full bg-white mt-4 mb-6"></div>
+            <div className="flex">
+                {/* Left */}
+                <div className="w-full">
+                    {/* Options */}
+                    {/* <div className=" h-9 flex justify-between mb-3">
+                        <div className="bg-white placeholder_animation h-full w-1/6"></div>
+                        <div className="flex h-full justify-between w-9/12">
+                            <div className="bg-white placeholder_animation h-full w-1/6"></div>
+                            <div className="bg-white placeholder_animation h-full w-1/6"></div>
+                            <div className="bg-white placeholder_animation h-full w-1/6 "></div>
+                            <div className="bg-white placeholder_animation h-full w-1/6"></div>
+                        </div>
+                    </div> */}
+                    {/* Categories */}
+                    {/* <div className="placeholder_animation h-36 bg-white my-4"></div> */}
+
+                    <div className="grid grid-cols-2 gap-3">
+                        <div className="placeholder_animation h-72 bg-white"></div>
+                        <div className="placeholder_animation h-72 bg-white"></div>
+                        <div className="placeholder_animation h-72 bg-white"></div>
+                        <div className="placeholder_animation h-72 bg-white"></div>
+                        <div className="placeholder_animation h-72 bg-white"></div>
+                        <div className="placeholder_animation h-72 bg-white"></div>
+                        <div className="placeholder_animation h-72 bg-white"></div>
+                        <div className="placeholder_animation h-72 bg-white"></div>
+                        <div className="placeholder_animation h-72 bg-white"></div>
+                        <div className="placeholder_animation h-72 bg-white"></div>
+                        <div className="placeholder_animation h-72 bg-white"></div>
+                        <div className="placeholder_animation h-72 bg-white"></div>
+                        <div className="placeholder_animation h-72 bg-white"></div>
+                        <div className="placeholder_animation h-72 bg-white"></div>
+                        <div className="placeholder_animation h-72 bg-white"></div>
+                        <div className="placeholder_animation h-72 bg-white"></div>
+                        <div className="placeholder_animation h-72 bg-white"></div>
+                        <div className="placeholder_animation h-72 bg-white"></div>
+                        <div className="placeholder_animation h-72 bg-white"></div>
+                        <div className="placeholder_animation h-72 bg-white"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </>
+
+
+    
     )
 }
 export default CatalogPlaceholder
