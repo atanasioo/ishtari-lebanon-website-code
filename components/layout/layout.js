f19a456ad5cd5e1d77d275c01c781901cf8f2525
rabie234 <kwayderr@gmail.com>
2024-01-22 09:30:59 +0200

    //

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -48,28 +48,28 @@ function Layout({
   }, [router.asPath]);
 
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollTop = window.pageYOffset;
-      if(scrollTop<200){
-        setisShowHeader(true)
-      }else{
-        if(scrollTop> prevScroll){
-          //  console.log(`prev${prevScroll}`);
-            setisShowHeader(false)
-          }else{
-            setisShowHeader(true)
-          }
-        prevScroll = scrollTop
-      }
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const scrollTop = window.pageYOffset;
+  //     if(scrollTop<200){
+  //       setisShowHeader(true)
+  //     }else{
+  //       if(scrollTop> prevScroll){
+  //         //  console.log(`prev${prevScroll}`);
+  //           setisShowHeader(false)
+  //         }else{
+  //           setisShowHeader(true)
+  //         }
+  //       prevScroll = scrollTop
+  //     }
      
-    };
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      // Clean up the event listener when the component is unmounted
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+  //   };
+  //   window.addEventListener('scroll', handleScroll);
+  //   return () => {
+  //     // Clean up the event listener when the component is unmounted
+  //     window.removeEventListener('scroll', handleScroll);
+  //   };
+  // }, []);
   // console.log(information_data.informations)
   // console.log("token inlayout " +token);
   // useEffect(() => {
