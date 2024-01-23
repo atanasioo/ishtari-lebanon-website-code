2b94d432bbc9ef0896e2fbe2b3075a0a8e248d98
rabie234 <kwayderr@gmail.com>
2024-01-23 10:33:49 +0200

    //

---
+++ pages/cartShared.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import React from 'react'
+
+const CartShared = () => {
+  return (
+    <div className='min-h-screen'>
+
+<Head>
+          <title> Shopping Cart | ishtari</title>
+          <meta
+            name="description"
+            content="Review and finalize your order with our convenient and secure shopping cart. Add items, adjust quantities, and proceed to checkout with confidence. Enjoy hassle-free online shopping and quick delivery of your favorite products."
+          ></meta>
+        </Head>
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default CartShared
